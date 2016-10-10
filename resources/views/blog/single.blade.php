4dfe3c147c384d703294f65da85fecf2e3c4ad1a
Jonas Adnan <adnan@snekkerbua.local>
2016-10-10 12:52:06 +0200

    test git - added space to file

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -5,6 +5,7 @@
     @section('content')
 
             <div class="row">
+                
                 <div class="col-md-8 col-md-offset-2 ">
 
                     <h1>{{ $post->title }}</h1>
